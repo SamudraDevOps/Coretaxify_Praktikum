de1fca08f30d57b88da49c02e76224918d9b735f
Riandika Ramadhan <73620190+dikamantab@users.noreply.github.com>
2025-02-17 09:24:55 +0700

    fix

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -43,9 +43,9 @@ const Header = () => {
                                                             {[
                                                                       { label: "Portal Saya", submenu: ["Dokumen Saya", "Notifikasi Saya", "Kasus Saya", "Kasus Berjalan Saya", "Profil Saya", "Pengukuhan PKP", "Pendaftaran Objek Pajak PBB P5L", { label: "Perubahan Data", submenu: ["Perubahan Data", "Pengukuhan PKP", "Pendaftaran Objek Pajak PBB P5L"] }, { label: "Perubahan Status", submenu: ["Penetapan Wajib Pajak Nonaktif", "Pengaktifan Kembali Wajib Pajak Nonaktif", "Penunjuk Pemungut PMSE Dalam Negeri", "Penetapan Pemungut Bea Materai", "Pencabutan Pemungut Bea Materai", "Penunjukan Pemotong atau Pemungut PPh/PPN", "Pencabutan Pemotong atau Pemungut PPh/PPN", "Pencabutan Pemungut PPN PMSE", "Lembaga Keuangan Pelapor - Penetapan", "Lembaga Keuangan Pelapor - Pencabutan", "Lembaga Keuangan Pelapor - Perubahan Data"] }, "Pengahpusan & Pencabutan", "Profil Institusi Finansial"] },
                                                                       { label: "E-Faktur", submenu: [] },
-                                                                      { label: "e-Bupot", submenu: ["Lihat Bupot", "Buat Bupot"] },
-                                                                      { label: "Surat Pemberitahuan(SPT)", submenu: ["SPT Tahunan", "SPT Masa"] },
-                                                                      { label: "Pembayaran", submenu: ["Cek Pembayaran", "Riwayat"] },
+                                                                      { label: "e-Bupot", submenu: ["BPPU", "BPNR", "Penyetoran Sendiri", "Pemotongan Secara Digunggung", "BP 21 - Bukti Pemotongan Selain Pegawai Tetap", "BP 26 - Bukti Pemotongan Wajib Pajak Luar Negeri", "BP A1 - Bukti Pemotongan A1 Masa Pajak Terakhir", "BP A2 - Bukti Pemotongan A1 Masa Pajak Terakhir", "Bukti Pemotongan Bulanan Pegawai Tetap", "Unggah Dokumen Yang Dipersamakan dengan Bukti Pemotongan/Pemungutan"] },
+                                                                      { label: "Surat Pemberitahuan(SPT)", submenu: ["Surat Pemberitahuan (SPT)", "Pencatatan", "Dasbor Kompensasi", "Pengungkapan Ketidakbenaran SPT"] },
+                                                                      { label: "Pembayaran", submenu: ["Permohonan Pemindahbukuan", "Layanan Mandiri Kode Billing", "Pembuatan Kode Billing atas Tagihan Pajak", "Daftar Kode Billing Belum Dibayar", "Formulir Restitusi Pajak", "Permohonan Pemberian Imbalan Bunga", "Permohonan PPh DTP atas Penghasilan PDAM"] },
                                                                       { label: "Buku Besar", submenu: [] },
                                                             ].map((item, index) => (
                                                                       <li key={index} className="relative">
@@ -65,9 +65,9 @@ const Header = () => {
                                                                                           </button>
                                                                                 )}
                                                                                 {item.submenu.length > 0 && dropdownOpen === item.label && (
-                                                                                          <ul className="absolute left-0 mt-2 w-[300px] bg-blue-900 text-white shadow-md rounded-md">
+                                                                                          <ul className="absolute left-0 mt-3 min-w-max bg-blue-900 text-white shadow-md rounded-md">
                                                                                                     {item.submenu.map((sub, subIndex) => (
-                                                                                                              <li key={subIndex} className="relative px-4 py-4 hover:bg-yellow-500 cursor-pointer">
+                                                                                                              <li key={subIndex} className="relative px-4 py-4 hover:bg-yellow-500 cursor-pointer whitespace-nowrap">
                                                                                                                         {typeof sub === "string" ? (
                                                                                                                                   <button onClick={() => navigateTo(sub)}>{sub}</button>
                                                                                                                         ) : (
@@ -79,9 +79,9 @@ const Header = () => {
                                                                                                                                                       {sub.label} <ChevronRight className="w-4 h-4 ml-2" />
                                                                                                                                             </button>
                                                                                                                                             {sub.submenu && subDropdownOpen === sub.label && (
-                                                                                                                                                      <ul className="absolute left-full mx-1 top-0 mt-0 w-[450px] bg-blue-900 text-white shadow-md rounded-md text-left">
+                                                                                                                                                      <ul className="absolute left-full mx-1 top-0 mt-0 min-w-max bg-blue-900 text-white shadow-md rounded-md text-left">
                                                                                                                                                                 {sub.submenu.map((nestedSub, nestedIndex) => (
-                                                                                                                                                                          <li key={nestedIndex} className="px-4 py-2 hover:bg-yellow-500 cursor-pointer">
+                                                                                                                                                                          <li key={nestedIndex} className="px-4 py-2 hover:bg-yellow-500 cursor-pointer whitespace-nowrap">
                                                                                                                                                                                     <button onClick={() => navigateTo(nestedSub)}>{nestedSub}</button>
                                                                                                                                                                           </li>
                                                                                                                                                                 ))}
@@ -92,6 +92,7 @@ const Header = () => {
                                                                                                               </li>
                                                                                                     ))}
                                                                                           </ul>
+
                                                                                 )}
                                                                       </li>
                                                             ))}
