492d56b6cbe4612ec90138feeee25757c1d37777
Riandika Ramadhan <73620190+dikamantab@users.noreply.github.com>
2025-05-08 12:58:49 +0700

    update

--- src/components/PraktikumPage/Badan/BUPOT/CreateEbupot.jsx
+++ src/components/PraktikumPage/Badan/BUPOT/CreateEbupot.jsx
@@ -13,8 +13,31 @@ const CreateEbupot = () => {
     const [pegawaiAsing, setPegawaiAsing] = useState('');
     const [selectedNegara, setSelectedNegara] = useState(null);
     const [selectedStatusPTKP, setSelectedStatusPTKP] = useState(null);
+    const [namaObjekPajak, setNamaObjekPajak] = useState('');
+    const [kodeObjekPajak, setKodeObjekPajak] = useState('');
+    const [penghasilanBruto, setPenghasilanBruto] = useState('');
+    const [penghasilanBrutoRaw, setPenghasilanBrutoRaw] = useState(0);
+    const [fasilitasPajak, setFasilitasPajak] = useState('');
+
+
+    const formatRupiah = (value) => {
+        const numberString = value.replace(/[^\d]/g, '');
+        return new Intl.NumberFormat('id-ID').format(numberString);
+    };
+
+    const handlePenghasilanChange = (e) => {
+        const rawInput = e.target.value.replace(/[^\d]/g, '');
+        const formatted = new Intl.NumberFormat('id-ID').format(rawInput);
+
+        setPenghasilanBruto(formatted);        // tampil di input (pakai titik)
+        setPenghasilanBrutoRaw(Number(rawInput)); // nilai bersih (tanpa titik)
+    };
+    
+
+
 
     return (
+        
         <div className="flex h-screen bg-gray-100">
             <div className="flex-auto p-3 bg-white rounded-md h-full">
                 <div className="flex justify-between items-center mb-6">
@@ -219,11 +242,134 @@ const CreateEbupot = () => {
                             <label className="block text-sm font-medium text-gray-700">
                                 Fasilitas Pajak yang dimiliki penyetor<span className="text-red-500">*</span>
                             </label>
-                            <select className='w-full border p-2 rounded'>
+                            <select
+                                className='w-full border p-2 rounded'
+                                value={fasilitasPajak}
+                                onChange={(e) => setFasilitasPajak(e.target.value)}
+                            >
                                 <option value="">Please Select</option>
                                 <option value="Fasilitas Lainnya">Fasilitas Lainnya</option>
                                 <option value="Ditanggung Pemerintah">PPh Ditanggung Pemerintah (DTP)</option>
+                                <option value="Tanpa Fasilitas">Tanpa Fasilitas</option>
                             </select>
+                        </div>
+                        <div className="mt-4">
+                            <label className="block text-sm font-medium text-gray-700">
+                                Nama Objek Pajak<span className="text-red-500">*</span>
+                            </label>
+                            <select
+                                className="w-full border p-2 rounded"
+                                value={namaObjekPajak}
+                                onChange={(e) => {
+                                    const selectedValue = e.target.value;
+                                    setNamaObjekPajak(selectedValue);
+                                    switch (selectedValue) {
+                                        case "Penghasilan yang diterima atau diperoleh pegawai tetap":
+                                            setKodeObjekPajak("21-100-01");
+                                            break;
+                                        case "Penghasilan yang diterima atau diperoleh secara teratur":
+                                            setKodeObjekPajak("21-100-02");
+                                            break;
+                                        case "Penghasilan yang diterima atau diperoleh Pegawai tetap yang menerima fasilitas di daerah tertentu":
+                                            setKodeObjekPajak("21-100-03");
+                                            break;
+                                        default:
+                                            setKodeObjekPajak("");
+                                    }
+                                }}
+                            >
+                                <option value="">Please Select</option>
+                                <option value="Penghasilan yang diterima atau diperoleh pegawai tetap">
+                                    Penghasilan yang diterima atau diperoleh pegawai tetap
+                                </option>
+                                <option value="Penghasilan yang diterima atau diperoleh secara teratur">
+                                    Penghasilan yang diterima atau diperoleh secara teratur
+                                </option>
+                                <option value="Penghasilan yang diterima atau diperoleh Pegawai tetap yang menerima fasilitas di daerah tertentu">
+                                    Penghasilan yang diterima atau diperoleh Pegawai tetap yang menerima fasilitas di daerah tertentu
+                                </option>
+                            </select>
+                        </div>
+                        <div className="mt-4">
+                            <label className="block text-sm font-medium text-gray-700">
+                                Jenis Pajak
+                            </label>
+                            <input type="text" className="w-full border p-2 rounded bg-gray-100" value="Pasal 21" disabled />
+                        </div>
+                        <div className="mt-4">
+                            <label className="block text-sm font-medium text-gray-700">
+                                Kode Objek Pajak
+                            </label>
+                            <select
+                                className="w-full border p-2 rounded bg-gray-100"
+                                value={kodeObjekPajak}
+                                onChange={(e) => setKodeObjekPajak(e.target.value)}
+                                disabled
+                            >
+                                <option value="">Please Select</option>
+                                <option value="21-100-01">21-100-01</option>
+                                <option value="21-100-02">21-100-02</option>
+                                <option value="21-100-03">21-100-03</option>
+                            </select>
+                        </div>
+                        <div className="mt-4">
+                            <label className="block text-sm font-medium text-gray-700">
+                                Penghasilan Bruto (RP)<span className="text-red-500">*</span>
+                            </label>
+                            <input
+                                type="text"
+                                className="w-full border p-2 rounded"
+                                placeholder="Masukkan Penghasilan Bruto"
+                                value={penghasilanBruto}
+                                onChange={handlePenghasilanChange}
+                                onWheel={(e) => e.target.blur()}
+                            />
+                        </div>
+                        <div className="mt-4">
+                            <label className="block text-sm font-medium text-gray-700">
+                                Tarif (%) <span className="text-red-500">*</span>
+                            </label>
+                            <input
+                                type="number"
+                                className={`w-full border p-2 rounded ${["Ditanggung Pemerintah", "Tanpa Fasilitas"].includes(fasilitasPajak) ? "bg-gray-100" : "bg-white"}`}
+                                placeholder="Masukkan Tarif"
+                                onWheel={(e) => e.target.blur()}
+                                disabled={["Ditanggung Pemerintah", "Tanpa Fasilitas"].includes(fasilitasPajak)}
+                            />
+                        </div>
+                        <div className="mt-4">
+                            <label className="block text-sm font-medium text-gray-700">
+                                Penghasilan yang dipotong <span className="text-red-500">*</span>
+                            </label>
+                            <input
+                                type="number"
+                                className={`w-full border p-2 rounded ${["Ditanggung Pemerintah", "Tanpa Fasilitas"].includes(fasilitasPajak) ? "bg-gray-100" : "bg-white"}`}
+                                placeholder="Masukkan Penghasilan yang dipotong"
+                                onWheel={(e) => e.target.blur()}
+                                disabled={["Ditanggung Pemerintah", "Tanpa Fasilitas"].includes(fasilitasPajak)}
+                            />
+                        </div>
+                        <div className="mt-4">
+                            <label className='block text-sm font-medium text-gray-700'>
+                                KAP
+                            </label>
+                            <input 
+                                type="text" 
+                                className='w-full border p-2 rounded bg-gray-100' 
+                                disabled
+                                value="411121-100"
+                            />
+                        </div>
+                        <div className="mt-4">
+                            <label className='block text-sm font-medium text-gray-700'>
+                                NITKU/Nomor Identitas Sub Unit Organisasi <span className='text-red-500'>*</span>
+                            </label>
+                            <input
+                                type="text"
+                                className='w-full border p-2 rounded bg-gray-100'
+                                disabled
+                                value="Badan"
+                            />
                         </div>
                     </div>
                 )}
