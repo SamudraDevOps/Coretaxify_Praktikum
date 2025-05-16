85ebc4c714fa93c86ad6119158344e18f9ae34a0
Riandika Ramadhan <73620190+dikamantab@users.noreply.github.com>
2025-05-16 16:56:07 +0700

    update

--- src/components/PraktikumPage/Badan/SPT/CreateKonsepPasal.jsx
+++ src/components/PraktikumPage/Badan/SPT/CreateKonsepPasal.jsx
@@ -31,6 +31,32 @@ const CreateKonsepPasal = () => {
                 <div className="flex justify-between items-center mb-6">
                     <h2 className="text-3xl font-light text-yellow-500 mt-4">PEMOTONGAN PPH PASAL 21 DAN ATAU PASAL 26</h2>
                 </div>
+                <div className="w-full p-2 ml-0 border-t text-lg">
+                    <Tabs defaultValue='induk' onValueChange={(val) => setActiveTab(val)} >
+                        <TabsList className="flex justify-start gap-2 text-blue-700 text-lg">
+                            <TabsTrigger value="induk">Induk</TabsTrigger>
+                            <TabsTrigger value="a-1">A-1</TabsTrigger>
+                            <TabsTrigger value="a-2">A-2</TabsTrigger>
+                            <TabsTrigger value="b-1">B-1</TabsTrigger>
+                            <TabsTrigger value="b-2">B-2</TabsTrigger>
+                            <TabsTrigger value="b-3">B-3</TabsTrigger>
+                            <TabsTrigger value="c">C</TabsTrigger>
+                        </TabsList>
+                        <TabsContent value="induk">
+                            <div className="mt-4">
+                                <div className="border rounded-md p-4 mb-2 cursor-pointer flex justify-between items-center bg-gray-100 w-full" onClick={() => setShowHeaderInduk(!showHeaderInduk)}>
+                                    <h3 className='text-lg font-semibold'>Header</h3>
+                                    {showHeaderInduk ? <FaChevronUp /> : <FaChevronDown />}
+                                </div>
+                                {showHeaderInduk && (
+                                    <div className="border rounded-md p-4 mb-2 cursor-pointer flex justify-between items-center bg-gray-100 w-full">
+                                        <h3 className='text-lg font-semibold'>Header</h3>
+                                    </div>
+                                )}
+                            </div>
+                        </TabsContent>
+                    </Tabs>
+                </div>
             </div>
         </div>
     )
