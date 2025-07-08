79191413e7fea5a1354b9bff1e763e0bae7ce25f
GalihPrev <galihpreviand27@gmail.com>
2025-07-08 10:48:53 +0700

    PPn jadi PPN

--- src/components/PraktikumPage/Badan/SPT/BuatKonsepSPT.jsx
+++ src/components/PraktikumPage/Badan/SPT/BuatKonsepSPT.jsx
@@ -191,7 +191,7 @@ const BuatKonsepSPT = ({ sidebar }) => {
   };
 
   const taxTypes = [
-    { label: "PPn", value: "ppn" },
+    { label: "PPN", value: "ppn" },
     { label: "PPh Pasal 21/26", value: "pph" },
     { label: "PPh Unifikasi", value: "pphunifikasi" },
     { label: "PPh Badan", value: "badan" },
