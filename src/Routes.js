c088bd2bf030994f76b21c78fd968b361f2c7a95
Muhamad Adi Setyawan <ggmild55@gmail.com>
2025-09-07 02:10:22 +0000

    update: set api url to api.coretaxify.com

--- src/Routes.js
+++ src/Routes.js
@@ -5,10 +5,8 @@ import { IntentEnum } from "./enums/IntentEnum";
 // const url = "http://127.0.0.1:8000/";
 // const apiUrl = "http://127.0.0.1:8000/api/";
 
-// const url = "https://api.coretaxify.com/";
-// const apiUrl = "https://api.coretaxify.com/api/";
-const url = "https://coretaxify-api.venturo.id/";
-const apiUrl = "https://coretaxify-api.venturo.id/api/";
+const url = "https://api.coretaxify.com/";
+const apiUrl = "https://api.coretaxify.com/api/";
 // const url = "http://127.0.0.1:8004/";
 // const apiUrl = "http://127.0.0.1:8004/api/";
 const role = {
