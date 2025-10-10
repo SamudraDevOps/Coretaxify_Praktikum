3ecb54336ca6258793659b40621dbee60ae21679
GalihPrev <galihpreviand27@gmail.com>
2025-10-10 14:37:23 +0700

    delete

--- src/components/PraktikumPage/Badan/SPT/Spt_Pribadi/Lampiran/sections/BagianA/index.jsx
+++
@@ -1,7 +0,0 @@
-export { default as KasdanSetaraKas } from "./KasdanSetarakas"; // ← Perhatikan huruf besar K di "Kas"
-export { default as Piutang } from "./Piutang";
-export { default as Investasi } from "./Investasi";
-export { default as HartaBergerak } from "./HartaBergerak";
-export { default as HartaTidakBergerak } from "./HartaTidakBergerak";
-export { default as HartaLainnya } from "./HartaLainnya";
-export { default as Ikhtisar } from "./Ikhtisar";
