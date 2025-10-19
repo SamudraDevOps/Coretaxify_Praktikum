6972e526229c007075c2fdc208c90903391c5dcc
GalihPrev <galihpreviand27@gmail.com>
2025-10-19 12:29:48 +0700

    Lampiran 3 Dinamis Field Fix

--- src/components/PraktikumPage/Badan/SPT/Spt_Pribadi/Lampiran/Lampiran_3A1.jsx
+++ src/components/PraktikumPage/Badan/SPT/Spt_Pribadi/Lampiran/Lampiran_3A1.jsx
@@ -1,13 +1,11 @@
 import React, { useState } from "react";
 import { FaChevronDown, FaChevronUp } from "react-icons/fa";
 import HeaderLampiran from "./HeaderLampiran";
-
-// pastikan path ini benar sesuai struktur kamu
-// kalau container utama LaporanLabaRugi ada di index.jsx (Lampiran3A-1)
-import Lampiran3A1 from "@sections/Lampiran3/Lampiran3A-1/BagianA";
+import LabaRugi from "@sections/Lampiran3/Lampiran3A-1/BagianA";
 
 export default function Lampiran_3A1({ data }) {
   const [showBagianA, setShowBagianA] = useState(true);
+  const [showBagianB, setShowBagianB] = useState(true);
 
   return (
     <div className="space-y-4">
@@ -19,9 +17,7 @@ export default function Lampiran_3A1({ data }) {
           className="border rounded-md p-4 mb-2 cursor-pointer flex justify-between items-center bg-gray-100 w-full"
           onClick={() => setShowBagianA(!showBagianA)}
         >
-          <h3 className="text-lg font-semibold">
-            A.1. LAPORAN LABA RUGI
-          </h3>
+          <h3 className="text-lg font-semibold">A.1. LAPORAN LABA RUGI</h3>
           {showBagianA ? <FaChevronUp /> : <FaChevronDown />}
         </div>
 
@@ -29,7 +25,27 @@ export default function Lampiran_3A1({ data }) {
           <div className="border rounded-md p-4 space-y-4">
             <div className="ml-4">
               {/* tampilkan seluruh form + tabel di sini */}
-              <Lampiran3A1 />
+              <LabaRugi />
+            </div>
+          </div>
+        )}
+      </div>
+
+      {/* Bagian A - Laporan Laba Rugi */}
+      <div>
+        <div
+          className="border rounded-md p-4 mb-2 cursor-pointer flex justify-between items-center bg-gray-100 w-full"
+          onClick={() => setShowBagianB(!showBagianB)}
+        >
+          <h3 className="text-lg font-semibold">A.2. LAPORAN POSISI KEUANGAN (NERACA)</h3>
+          {showBagianB ? <FaChevronUp /> : <FaChevronDown />}
+        </div>
+
+        {showBagianB && (
+          <div className="border rounded-md p-4 space-y-4">
+            <div className="ml-4">
+              {/* tampilkan seluruh form + tabel di sini */}
+              {/* <Lampiran3A1 /> */}
             </div>
           </div>
         )}
