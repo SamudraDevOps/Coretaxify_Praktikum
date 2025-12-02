2537092e0036aee2b796707c094422671c2ed162
GalihPrev <galihpreviand27@gmail.com>
2025-12-02 17:03:19 +0700

    Delete File Tidak Digunakan

--- src/components/PraktikumPage/Badan/SPT/Spt_Pribadi/Lampiran/components/Lampiran3B/RekapitulasiPajakFinal/index.jsx
+++
@@ -1,5 +0,0 @@
-export { default as TablePajakFinal } from "./TablePajakFinal";
-export { default as ModalPajakFinal } from "./form/ModalPajakFinal";
-export { default as FormFieldPajakFinal } from "./form/FormFieldPajakFinal";
-export { buildSchema, isFieldReadonly, BULAN_FIELDS, BULAN_NAMES } from "./form/schemas";
-export { calculateRowTotal, updateTotalRow, calculateGrandTotal } from "./form/helper";
